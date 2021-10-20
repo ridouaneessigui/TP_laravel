500c72e4206402ce49ecf12cffa82779598258dc
User Insta <ridouaneessigui@gmail.com>
2021-10-20 16:53:48 +0200

    Question 8

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
+
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -25,4 +26,18 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
+    $factory->define(Contact::class, function (Faker $faker) {
+        return [
+            'fullname' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'phone'  => phoneNumber(),
+            'address' => address(),
+            'datenaissance' =>date($format = 'd-m-y', $max = 'now'),
+        ];
+        $factory->define(Contact::class, function (Faker $faker) {
+            return [
+                'name' => $faker->name,
+            ];
 });
